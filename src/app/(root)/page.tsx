e2e1ac5ed2aa9fd637da85262b4835504a8ec6ec
reda.apps.4.fun@gmail.com <reda.apps.4.fun@gmail.com>
2025-01-26 10:47:45 +0530

    Second commit

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-work-sans font-bold">HOME</h1>
+    </div>
+  );
+}
---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,6 @@
+import { auth, handlers } from "../../../../../auth" // Referring to the auth.ts we just created
+export const { GET, POST } = handlers
+
+export { auth as middleware } from "../../../../../auth"
+
+
